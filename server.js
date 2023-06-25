c5439aff098bf9ec5c5701cba00c443598049c53
Hussein Najah <husseinnajah123@gmail.com>
2023-06-26 00:10:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //! Initialize express
 const express = require("express");
 const studentRoute = require("./src/student/routes");
+const userRoute = require("./src/user/routes");
 
 const app = express();
 const port = 3000;
@@ -8,9 +9,18 @@ const port = 3000;
 app.use(express.json());
 
 //! Routes
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
 //! V1 Api routes
+
 app.use("/api/v1/students", studentRoute);
+
+//!
+app.use("/api/v1/users",userRoute);
+
+//! 404 Not found
+app.use("*", (req, res) => res.status(404).send("404 Not found"));
 
 //! Listen on port 3000
 app.listen(port, () =>
